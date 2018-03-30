2252d9ecb6fe59df37f48a40526f8c1161ab1d54
Charlotte Payne <cltpayne@gmail.com>
2018-03-30 12:16:40 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,43 @@
+def display_board (board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(string)
+  string.to_i - 1 
+end
+
+def valid_move?(board, index)
+  if index.between?(0,8) && position_taken?(board,index) == false
+    return true
+  else 
+    return false
+  end
+end
+
+def position_taken?(board, index)
+  if (board[index] == "" || board[index] == " " || board[index] == nil)
+    return false
+  else 
+    return true
+  end 
+end
+
+def move(board, index, token="X")
+  board[index] = token
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  index = gets.chomp
+  input_to_index(index)
+  if valid_move? == false 
+    puts "Please enter 1-9:"
+  else 
+    move
+  end 
+end
+
